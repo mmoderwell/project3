eede288514aecf13b83a702ca4e0515f41d1e31f
Matt Moderwell <mmoderwell@gmail.com>
2019-11-05 10:25:25 -0600

    Map working and coverted to v5

--- frontend/main.js
+++ frontend/main.js
@@ -1,79 +1,76 @@
-// set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-	width = 460 - margin.left - margin.right,
-	height = 400 - margin.top - margin.bottom;
+// The svg
+var svg = d3.select("svg"),
+	width = +svg.attr("width"),
+	height = +svg.attr("height");
 
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-	.append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform",
-		"translate(" + margin.left + "," + margin.top + ")");
+// Map and projection
+var path = d3.geoPath();
+var projection = d3.geoMercator()
+	.scale(70)
+	.center([0, 20])
+	.translate([width / 2, height / 2]);
 
-//Read the data
-d3.csv("http://localhost:2000/flowers", function (data) {
-	// Add X axis
-	var x = d3.scaleLinear()
-		.domain([4, 8])
-		.range([0, width]);
-	svg.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x));
-
-	// Add Y axis
-	var y = d3.scaleLinear()
-		.domain([0, 9])
-		.range([height, 0]);
-	svg.append("g")
-		.call(d3.axisLeft(y));
-
-	// Color scale: give me a specie name, I return a color
-	var color = d3.scaleOrdinal()
-		.domain(["setosa", "versicolor", "virginica"])
-		.range(["#440154ff", "#21908dff", "#fde725ff"])
+// Data and color scale
+var data = d3.map();
+var colorScale = d3.scaleThreshold()
+	.domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+	.range(d3.schemeBlues[7]);
 
 
-	// Highlight the specie that is hovered
-	var highlight = function (d) {
+let promises = [];
 
-		selected_specie = d.Species
+promises.push(d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"));
+promises.push(d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv"));
 
-		d3.selectAll(".dot")
-			.transition()
-			.duration(200)
-			.style("fill", "lightgrey")
-			.attr("r", 3)
 
-		d3.selectAll("." + selected_specie)
-			.transition()
-			.duration(200)
-			.style("fill", color(selected_specie))
-			.attr("r", 7)
-	};
-
-	// Highlight the specie that is hovered
-	var doNotHighlight = function () {
-		d3.selectAll(".dot")
-			.transition()
-			.duration(200)
-			.style("fill", "lightgrey")
-			.attr("r", 5)
-	};
-
-	// Add dots
-	svg.append('g')
-		.selectAll("dot")
-		.data(data)
-		.enter()
-		.append("circle")
-		.attr("class", function (d) { return "dot " + d.Species })
-		.attr("cx", function (d) { return x(d.Sepal_Length); })
-		.attr("cy", function (d) { return y(d.Petal_Length); })
-		.attr("r", 5)
-		.style("fill", function (d) { return color(d.Species) })
-		.on("mouseover", highlight)
-		.on("mouseleave", doNotHighlight)
-
+Promise.all(promises).then(function (values) {
+	// console.log(values);
+	ready(null, values[0]);
 });
+
+function ready(error, topo) {
+
+	let mouseOver = function (d) {
+		d3.selectAll(".Country")
+			.transition()
+			.duration(200)
+			.style("opacity", .5)
+		d3.select(this)
+			.transition()
+			.duration(200)
+			.style("opacity", 1)
+			.style("stroke", "black")
+	}
+
+	let mouseLeave = function (d) {
+		d3.selectAll(".Country")
+			.transition()
+			.duration(200)
+			.style("opacity", .8)
+		d3.select(this)
+			.transition()
+			.duration(200)
+			.style("stroke", "transparent")
+	}
+
+	// Draw the map
+	svg.append("g")
+		.selectAll("path")
+		.data(topo.features)
+		.enter()
+		.append("path")
+		// draw each country
+		.attr("d", d3.geoPath()
+			.projection(projection)
+		)
+		// set the color of each country
+		.attr("fill", function (d) {
+			d.total = data.get(d.id) || 0;
+			return colorScale(d.total);
+		})
+		.style("stroke", "transparent")
+		.attr("class", function (d) { return "Country" })
+		.style("opacity", .8)
+		.on("mouseover", mouseOver)
+		.on("mouseleave", mouseLeave)
+}
