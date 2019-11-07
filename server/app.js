4ea7eb713efcc65db71f2259e2992f6ee879daa9
Matt Moderwell <mmoderwell@gmail.com>
2019-11-07 10:20:50 -0600

    Changes to get ready for deployment

--- server/app.js
+++ server/app.js
@@ -11,20 +11,29 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.get('/geo', (req, res, next) => {
-	// res.sendFile('flowers.csv');
+app.use(express.static('../frontend/'));
+
+// app.get('*', function (req, res, next) {
+// 	console.log(req.url)
+// 	next()
+// });
+
+app.get('/', (req, res, next) => {
+	res.sendFile(path.resolve(__dirname, '../frontend/index.html'));
+});
+
+app.get('/api/geo', (req, res, next) => {
 	let file_path = path.join(__dirname, '/data', 'counties.json');
 	const contents = fs.readFileSync(file_path, 'utf8');
 	res.send(contents);
 });
 
-app.get('/data', (req, res, next) => {
-	// res.sendFile('flowers.csv');
+app.get('/api/data', (req, res, next) => {
 	let file_path = path.join(__dirname, '/data', 'county_data.csv');
 	const contents = fs.readFileSync(file_path, 'utf8');
 	res.send(contents);
 });
 
-app.listen(2000, () => {
-	console.log('Web server running on port 2000.');
+app.listen(2001, () => {
+	console.log('Web server running on port 2001.');
 });
